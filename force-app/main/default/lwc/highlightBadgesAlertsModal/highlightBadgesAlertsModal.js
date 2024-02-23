8deb4cfb15a48f18f84f07bcfbf4ce6cb176e903
Stefan Serkin <stefanserkin@gmail.com>
2024-02-22 22:26:25 -0500

    Comments and formatting

--- force-app/main/default/lwc/highlightBadgesAlertsModal/highlightBadgesAlertsModal.js
+++ force-app/main/default/lwc/highlightBadgesAlertsModal/highlightBadgesAlertsModal.js
@@ -3,8 +3,6 @@ import { LightningElement, api } from 'lwc';
 export default class HighlightBadgesAlertsModal extends LightningElement {
     @api modalHeader;
     @api alertMessages;
-    showCancel = false;
-    showOkay = true;
 
     handleCloseEvent() {
         this.dispatchEvent(new CustomEvent('close'));
